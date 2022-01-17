e2a5b93aaf9be73e8bb4400d1c0c11d02f785a04
rgioeli <rgioeli@icloud.com>
2022-01-17 15:11:16 -0500

    changed the text in app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New fotobox application!</Text>
+      <Text>Now this is something different!</Text>
       <StatusBar style="auto" />
     </View>
   );
